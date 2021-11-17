4a384239ca2b35d890012837ef5868e94edaa73f
CLEBERTIN-DO-GRAAL555 <92889323+CLEBERTIN-DO-GRAAL555@users.noreply.github.com>
2021-11-17 17:00:34 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 
+var nuvem,nuvemslakk;
 var trex ,trex_running;
 var chao,chaoimg
 function preload(){
+  nuvemslakk = loadImage("cloud.png")
   trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
 chaoimg = loadImage("ground2.png")
 }
@@ -24,13 +26,25 @@ function draw(){
   if(keyDown("space")){
     trex.velocityY = -10;
 }
-console.log(trex.y)
+//console.log(trex.y)
 trex.velocityY=trex.velocityY+0.8;
 chao.velocityX=-5
 trex.collide(chao)
 if(chao.x<0){
   chao.x=chao.width/2
 }
+gerarnuvens()
 drawSprites();
 
 }
+
+
+function gerarnuvens(){
+  if(frameCount    %  60 ==0 ){
+  var nuvem = createSprite(600,100,50,50)
+  nuvem.scale=0.5;
+  nuvem.y = Math.round(random(20,100));
+  nuvem.addImage(nuvemslakk)
+nuvem.velocityX =-2
+console.log(frameCount)
+}}
